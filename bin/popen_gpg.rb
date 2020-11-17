e4cc6f9de2e53b052b62b974b08effbbc519bc6e
Steeve McCauley <steeve.mccauley@gmail.com>
2020-11-17 08:05:45 -0500

    Some cleanup
    * accidentally referenced the wrong issue previously
    
    Issue #2

--- bin/popen_gpg.rb
+++ bin/popen_gpg.rb
@@ -14,23 +14,27 @@ object={
 	}
 }
 
+dest="/var/tmp/#{ME}.json.gpg"
+gpg="gpg -e -o #{dest}"
 begin
+	Open3.popen2e(gpg) do |stdin, stdout_stderr, wait_thread|
+	  Thread.new do
+	    stdout_stderr.each {|l| puts l }
+	  end
 
-gpg="egpg -e -o /var/tmp/#{ME}.gpg"
-Open3.popen2e(gpg) do |stdin, stdout_stderr, wait_thread|
-  Thread.new do
-    stdout_stderr.each {|l| puts l }
-  end
+	  stdin.puts JSON.pretty_generate(object)
+	  stdin.close
 
-  stdin.puts JSON.pretty_generate(object)
-  stdin.close
+	  wait_thread.value
 
-  wait_thread.value
-
-  puts "done"
+	  puts "object written to #{dest}"
 end
 
 rescue Errno::ENOENT => e
 	$stderr.puts "ERROR: gpg not found: #{gpg}"
+	exit 1
+rescue => e
+	$stderr.puts "#{e.class}: #{e.message}"
+	puts e.backtrace.join("\n")
 	exit 1
 end
